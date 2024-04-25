ee15d3b37dfe2e28a8f25b42cb008ae3b0fe99c5
alokMahapatra26 <alokmahapatra2604@gmail.com>
2024-04-26 04:52:18 +0530

    signin frontend done

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 function Signin() {
@@ -6,6 +6,48 @@ function Signin() {
 
   const navigate = useNavigate();
 
+  const [formData , setFormData] = useState({})
+  const [error , setError] = useState(null);
+  const [loading , setLoading] = useState(false);
+
+  const handleChange = (e:any)=> {
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+    })
+  }
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    
+
+    try {
+      setLoading(true)
+      const response = await fetch('/api/auth/signin', {
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if(data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return; 
+      }
+
+      setLoading(false);
+      setError(null)
+      navigate('/')
+    }catch (error:any) {
+      setLoading(false)
+      setError(error.message)
+      
+    }
+  }
+
+  console.log(formData)
   return (
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col ">
@@ -14,29 +56,32 @@ function Signin() {
       <br />
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
-      
+      <form className="card-body" onSubmit={handleSubmit}>
+  
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" onChange={handleChange} placeholder="email" className="input input-bordered" id='email' required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" onChange={handleChange} placeholder="password" className="input input-bordered" id='password' required />
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover" onClick={()=>{navigate('/sign-up')}}>Create an account</a>
+            <a href="#" className="label-text-alt link link-hover" onClick={()=>{navigate('/sign-in')}}>Create Account</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Sign in</button>
+          <button disabled={loading} className="btn btn-primary">{loading ? 'Loading...' : 'Signin'}</button>
         </div>
       </form>
+     
     </div>
+    {error && <p className="text-red-500">{error}</p>}
   </div>
+  
 </div>
   )
 }
